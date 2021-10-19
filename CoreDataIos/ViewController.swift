e9bc99c2dcbbf057719410a8f166bd62a7593a34
Ayodeji <ayodejiayankola@gmail.com>
2021-10-19 12:16:11 +0100

    New Names can be created, and this allow an existing name added to be editted and deleted

--- CoreDataIos/ViewController.swift
+++ CoreDataIos/ViewController.swift
@@ -9,35 +9,25 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController{
-
-    
     
     @IBOutlet weak var tableView: UITableView!
-    
     
     //Refrence to managed object context
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    
     //  Data for the table
     var items: [Person]?
-     
- 
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-//        (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
         tableView.dataSource = self
         tableView.delegate = self
         
         //Get items from Core Data
         fetchPeople()
-        
     }
-
+    
+    //MARK:- Fetch Data from core data
     func fetchPeople(){
         //fetch data from core data to display in the table view
         do {
@@ -49,100 +39,133 @@ class ViewController: UIViewController{
             
         }
     }
-    
-    
-    
-    
+
     
     @IBAction func didTapAddButton(_ sender: Any) {
-        print("Did tap add button")
         
+        //MARK:- Saving Data to coreData
+        
+        //Create Alert
         let alertController = UIAlertController(
             title: "Add Person",
             message: "What is the name of person ?",
             preferredStyle: .alert)
         alertController.addTextField()
+        
         //Configure button handler
-        let submitButton = UIAlertAction(title: "Add", style: .default) { (action) in
+        let submitButton = UIAlertAction(title: "Add", style: .default) { [self] (action) in
             //Get the textfield for the alert
             let textfield = alertController.textFields![0]
-            print("This is it\(textfield.text)")
+            //Create a person object
+            let newPerson = Person(context: self.context)
+            newPerson.name = textfield.text
+            newPerson.age = 30
+            newPerson.gender = "Male"
+            
+            // Save The Data
+            do {
+                try  self.context.save()
+            } catch {
+                //Error saving Data
+            }
+            
+            //Re-fetch the data
+            self.fetchPeople()
         }
         
-         //Add button
+        //Add button
         alertController.addAction(submitButton)
         //Show Alert
         self.present(alertController, animated: true, completion: nil)
     }
-    
-    
-    
-
-
 }
 
 
 
-// MARK: - UITableViewDelegate
+
+// MARK: - UITableViewDelegate - Edit Data in core data
 extension ViewController: UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let alertController = UIAlertController(
-//            title: "Update Person",
-//            message: "Please update the name of person in following field ?",
-//            preferredStyle: .alert)
-//        let personToUpdate = self.items?[indexPath.row]
-//
-//        alertController.addTextField { (textField) in
-//            if let name = personToUpdate?.name {
-//                textField.text = name
-//            }
-//        }
-//        alertController.addAction(.init(title: "Ok", style: .default, handler: { [weak self] (alertAction) in
-//            let textField = alertController.textFields?.first
-//
-//            personToUpdate?.name = textField?.text
-//
-////            self?.saveInCoreData()
-////
-////            self?.fetchPeople()
-//        }))
-//        self.present(alertController, animated: true)
-//    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Selected Person
+        let personToUpdate = self.items![indexPath.row]
+        
+        // Create alert
+        let alertController = UIAlertController(
+            title: "Update Person",
+            message: "Please update the name of person in following field ?",
+            preferredStyle: .alert)
+        alertController.addTextField()
+        let textfield = alertController.textFields![0]
+        textfield.text = personToUpdate.name
+        
+        // Configure button handler
+        let saveButton = UIAlertAction(title: "Save", style: .default) { (action) in
+            
+            // Get the textfield for the alert
+            let textfield =  alertController.textFields![0]
+            
+            // Edit name property of person object
+            personToUpdate.name = textfield.text
+            
+            // Save the data
+            do {
+                try self.context.save()
+            } catch {
+                
+            }
+            // Re-fetch the data
+            self.fetchPeople()
+        }
+        
+        // Add Button
+        alertController.addAction(saveButton)
+        
+        // show alert
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
 }
 
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         //Return the number of people
         return self.items?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(UITableViewCell.self)) ?? UITableViewCell()
-        
         let person = self.items?[indexPath.row]
-        cell.textLabel?.text = "person?.name"
+        cell.textLabel?.text = person?.name
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//        let action = UIContextualAction(
-//            style: .destructive,
-//            title: "Delete"
-//        ) { (action, view, completionHandler) in
-//            if let personToRemove = self.items?[indexPath.row] {
-//
-//                self.context.delete(personToRemove)
-////
-////                self.saveInCoreData()
-////
-//                print("Deleted: \(personToRemove.name ?? "")")
-//                self.fetchPeople()
-//            }
-//        }
-//
-//        return UISwipeActionsConfiguration(actions: [action])
-//    }
+    //MARK:- Delete Data from core data
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(
+            style: .destructive,
+            title: "Delete"
+        ) { (action, view, completionHandler) in
+            // Which person to be remove
+            let personToRemove = self.items![indexPath.row]
+            
+            //Remove the person
+            self.context.delete(personToRemove)
+            
+            // Save the data
+            do {
+                try self.context.save()
+            } catch {
+                
+            }
+            
+            // Refetch the data
+            self.fetchPeople()
+        }
+        
+        //Return swipe Actions
+        return UISwipeActionsConfiguration(actions: [action])
+    }
 }
 
